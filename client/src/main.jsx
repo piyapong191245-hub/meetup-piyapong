38bdd0012cda92583f1c7700e53f8069959253a5
digital-news-app <netloveyou12@gmail.com>
2026-09-25 12:28:03 +0700

    fix: update ClerkProvider in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if(!PUBLISHABLE_KEY){
   throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY in client environment");
-}
+}   
 
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/login">
